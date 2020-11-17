d88d9c4001f34c3a30c2215ace41f24e9701170a
BIll Ting <bting@lcs.on.ca>
2020-11-17 14:41:06 -0500

    Initial Commit

---
+++ Guessing numbers/Guessing_numbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guessing_numbersApp.swift
+//  Guessing numbers
+//
+//  Created by Bill Ting on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct Guessing_numbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
